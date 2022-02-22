13351a45c487afbc848142bf451c091093b7ba02
Borjaarmet <borjaarmet@gmail.com>
2022-02-22 12:55:41 +0100

    sidebar component

--- src/Hero.js
+++ src/Hero.js
@@ -8,14 +8,12 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-center">
-        <article hero-info>
+        <article className="hero-info">
           <h1>
-            {" "}
             Payments infrastructure <br />
             for the internet
           </h1>
           <p>
-            {" "}
             Millions of companies of all sizes—from startups to Fortune 500s—use
             Stripe’s software and APIs to accept payments, send payouts, and
             manage their businesses online.
